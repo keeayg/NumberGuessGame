db406da51044d40fea898caf928238d46e9b9924
keeay <keeay@KAANGRBZ687D>
2023-01-21 20:59:18 +0300

    Add project files.

---
+++ NumberGuessGame/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NumberGuessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool play = true;
+            Console.WriteLine("WELCOME");
+
+            while (play == true)
+            {
+                Console.WriteLine("Guess the number.");
+                int playerGuess = Convert.ToInt32(Console.ReadLine());
+                Random computerNumRND = new Random();
+                int computerChoice = computerNumRND.Next(1, 10);
+                if (playerGuess == computerChoice)
+                {
+                    Console.WriteLine($"Player Guess: {playerGuess}");
+                    Console.WriteLine($"Computer Choice: {computerChoice}");
+
+                    Console.WriteLine("CONGRATULATİONS! You guess right!");
+                } else
+                {
+                    Console.WriteLine("Try Again...");
+                }
+
+
+            }
+
+        }
+    }
+}
